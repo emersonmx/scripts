235e57c78a35c3549cedd4c64eadf37b4908699f
Emerson MX <emersonmx@gmail.com>
2013-05-07 09:32:31 -0300

    Removendo dados sensíveis do repositório.
    Signed-off-by: Emerson MX <emersonmx@gmail.com>

---
+++ star_login.sh
@@ -0,0 +1,12 @@
+#! /bin/bash
+
+if [ $# != 2 ]
+then
+    echo "Uso: $0 <usuário> <senha>"
+    exit 1
+fi
+
+curl -d "username="$1"&password="$2 \
+https://star.milhatelecom.com.br/login > /dev/null
+notify-send "Conectado!"
+
