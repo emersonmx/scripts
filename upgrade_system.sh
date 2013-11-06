4b3a3a55cd75c6fbd37a144f04dcaed6c499196a
Emerson MX <emersonmx@gmail.com>
2013-11-06 09:51:02 -0300

    Construir pacotes binários!
    Signed-off-by: Emerson MX <emersonmx@gmail.com>

--- upgrade_system.sh
+++ upgrade_system.sh
@@ -1,6 +1,7 @@
 #! /bin/bash
 
-emerge -NDuv @world &&
+emerge -NDuv @world \
+    --buildpkg --buildpkg-exclude "virtual/* sys-kernel/*-sources" &&
 emerge -v @preserved-rebuild &&
 emerge --depclean &&
 revdep-rebuild &&
