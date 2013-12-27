72b6eb5225e8b8404b1997367f84bc745f518f36
Emerson MX <emersonmx@gmail.com>
2013-12-27 03:04:57 -0300

    Adição de script para ser usando no QJackCtl quando se está usando o PulseAudio.
    Signed-off-by: Emerson MX <emersonmx@gmail.com>

---
+++ pulse_jack_pre_start.sh
@@ -0,0 +1,3 @@
+#! /bin/bash
+
+pacmd suspend true
---
+++ pulse_jack_pre_stop.sh
@@ -0,0 +1,8 @@
+#! /bin/bash
+
+SINKID=$(pactl list | grep -B 1 "Name: module-jack-sink" | grep Module | \
+    sed 's/[^0-9]//g')
+SOURCEID=$(pactl list | grep -B 1 "Name: module-jack-source" | grep Module | \
+    sed 's/[^0-9]//g')
+pactl unload-module $SINKID
+pactl unload-module $SOURCEID
