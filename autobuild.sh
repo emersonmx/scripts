22893709ccf2bd64ec9ffab1144a7d595d7b6eb0
Emerson MX <emersonmx@gmail.com>
2014-05-11 21:16:55 -0300

    Removendo scripts duplicados de outro repositório.
    Signed-off-by: Emerson MX <emersonmx@gmail.com>

--- autobuild.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-# Se houver mais de 1 arquivos .tex no diretório de execução, o latexmk não vai
-# saber o que fazer. Se for seu caso, você deve especificar o arquivo a ser
-# construido.
-latexmk -pvc -view=none -pdf # arquivo.tex
