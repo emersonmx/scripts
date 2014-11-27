f7c9ba5d0435fc09a16503f8829157110d88cbdc
Emerson MX <emersonmx@gmail.com>
2014-11-26 22:13:28 -0300

    Removendo compilação na memória.

--- upgrade_system.sh
+++ upgrade_system.sh
@@ -5,9 +5,9 @@ export SOLARIZED="true"
 echo "Baixando atualizações"
 eix-sync
 
-echo "Montando /var/tmp/portage na RAM"
-umount /var/tmp/portage
-mount -t tmpfs -o size=2048M,nr_inodes=1M tmpfs /var/tmp/portage
+#echo "Montando /var/tmp/portage na RAM"
+#umount /var/tmp/portage
+#mount -t tmpfs -o size=2048M,nr_inodes=1M tmpfs /var/tmp/portage
 
 echo "Atualizando o sistema"
 emerge -NDuv @world &&
@@ -17,5 +17,5 @@ revdep-rebuild &&
 emerge @module-rebuild &&
 dispatch-conf
 
-echo "Desmontando /var/tmp/portage"
-umount /var/tmp/portage
+#echo "Desmontando /var/tmp/portage"
+#umount /var/tmp/portage
