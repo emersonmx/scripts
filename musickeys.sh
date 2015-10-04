1f28e714d149f84f6a4143886ed9c4431aee06cd
Emerson MX <emersonmx@gmail.com>
2015-10-04 07:47:23 -0300

    Adição de script que da play/pause/next/prev usando as teclas multimídias.

---
+++ musickeys.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+#!/bin/sh
+
+case $1 in
+   "play")
+       key="XF86AudioPlay"
+       ;;
+   "next")
+       key="XF86AudioNext"
+       ;;
+   "prev")
+       key="XF86AudioPrev"
+       ;;
+   *)
+       echo "Usage: $0 play|next|prev"
+       exit 1
+        ;;
+esac
+xdotool key --window $(xdotool search --name "Spotify (Premium |Unlimited |Free )?- Linux Preview"|head -n1) $key
+exit 0
