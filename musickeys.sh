cc4166ff4bdf2d92bf75bcecb1d17528bcca8826
Emerson MX <emersonmx@gmail.com>
2016-02-18 21:45:02 -0300

    Correção para funcionar com o Spotify e o mpv.

--- musickeys.sh
+++ musickeys.sh
@@ -1,21 +1,25 @@
 #!/bin/bash
 
-#!/bin/sh
-
 case $1 in
-   "play")
-       action="PlayPause"
+   "play-pause")
+       dbusAction="PlayPause"
+       xdotAction="XF86AudioPlay"
        ;;
    "next")
-       action="Next"
+       dbusAction="Next"
+       xdotAction="XF86AudioNext"
        ;;
-   "prev")
-       action="Previous"
+   "previous")
+       dbusAction="Previous"
+       xdotAction="XF86AudioPrev"
        ;;
    *)
-       echo "Usage: $0 play|next|prev"
+       echo "Usage: $0 play-pause|next|previous"
        exit 1
         ;;
 esac
-dbus-send --print-reply --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.mpris.MediaPlayer2.Player.$action
+
+xdotool key --window $(xdotool search --name "mpv" | head -n 1) $xdotAction > /dev/null 2>&1
+dbus-send --print-reply --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.mpris.MediaPlayer2.Player.$dbusAction > /dev/null 2>&1
+
 exit 0
