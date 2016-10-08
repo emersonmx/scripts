41643e50db3430b11cce7e0d189ea49cac6be781
Emerson MX <emersonmx@gmail.com>
2016-10-07 23:14:00 -0300

    Update script wmctrl script

--- wmctrl-switch-by-application.sh
+++ wmctrl-switch-by-application.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
 
+direction="$1"
+
+case $direction in
+    next|previous)
+        ;;
+    *)
+        echo "Usage: $0 (next|previous)"
+        exit
+        ;;
+esac
+
 # get id of the focused window
 active_win_id=$(xprop -root | grep '^_NET_ACTIVE_W' | awk -F'# 0x' '{print $2}')
 
@@ -10,13 +21,22 @@ win_class=$(wmctrl -x -l | grep $active_win_id | awk '{print $2 " " $3}' )
 win_list=$(wmctrl -x -l | grep  "$win_class" | awk '{print $1}' )
 
 # get next window to focus on
-switch_to=$(echo $win_list | sed s/.*$active_win_id// | awk '{print $1}')
 
-# if the current window is the last in the list ... take the first one
-if [ -z "$switch_to" ]
-then
-    switch_to=$(echo $win_list | awk '{print $1}')
-fi
+switch_to=""
+case $direction in
+    next)
+        switch_to=$(echo $win_list | sed -r s/.*$active_win_id// | awk '{print $1}')
+        if [[ -z $switch_to ]]; then
+            switch_to=$(echo $win_list | awk '{print $1}')
+        fi
+        ;;
+    previous)
+        switch_to=$(echo $win_list | sed -r s/\\w+$active_win_id.*// | awk '{print $NF}')
+        if [[ -z $switch_to ]]; then
+            switch_to=$(echo $win_list | awk '{print $NF}')
+        fi
+        ;;
+esac
 
 # switch to window
 wmctrl -i -a $switch_to
