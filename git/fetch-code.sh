505d2c7d0ab620a6d31c68a39fe09aee96c88199
Emerson MX <emersonmx@gmail.com>
2017-10-06 13:18:10 -0300

    Move repositories to gitconfig

--- git/fetch-code.sh
+++ git/fetch-code.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 code_dir="$HOME/code"
-download_dirs="dotfiles my-arch-config phpd scripts"
+download_dirs="$(git config --global --get-all my-repositories.repository)"
 
 pushd $code_dir > /dev/null
 for dir in $download_dirs
