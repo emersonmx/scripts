1fddcc0e55815483cba378dfe3e74f225b3ec0db
Emerson MX <emersonmx@gmail.com>
2017-10-25 21:55:31 -0300

    Move to setup-laravel-dev.sh

--- laravel/setup-laravel-dev.sh
+++ laravel/setup-laravel-dev.sh
@@ -1,8 +1,31 @@
 #!/bin/bash
 
-# https://github.com/barryvdh/laravel-debugbar
-# https://github.com/barryvdh/laravel-ide-helper
+# Move User to App/Models
+if [ -f 'app/User.php' ]
+then
+    mkdir -p app/Models
+    mv app/User.php app/Models/
+    sed 's#^namespace App;#namespace App\\Models;#g' -i app/Models/User.php
+fi
 
+sed "s#'App\.User\.#'App.Models.User.#g" -i routes/channels.php
+sed 's#App\\User::class#App\\Models\\User::class#g' -i config/auth.php
+sed 's#App\\User::class#App\\Models\\User::class#g' -i config/services.php
+sed 's#App\\User::class#App\\Models\\User::class#g' -i database/factories/UserFactory.php
+sed 's#App\\User#App\\Models\\User#g' -i app/Http/Controllers/Auth/RegisterController.php
+
+# Create Services and Repositories
+mkdir -p app/{Services,Repositories}
+
+# Setup phpunit to use sqlite db in memory
+sed '
+/<env name="APP_ENV" value="testing"\/>/ a\
+        <env name="DB_CONNECTION" value="sqlite"/>\
+        <env name="DB_DATABASE" value=":memory:"/>
+' -i phpunit.xml
+
+
+# Setup dev packages
 composer require doctrine/dbal
 composer require --dev barryvdh/laravel-debugbar
 composer require --dev barryvdh/laravel-ide-helper
--- laravel/setup-services-and-repositories.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-mkdir -p app/{Services,Repositories}
