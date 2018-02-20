d0ecb619c71238fd5c457da071bae173a511c2ef
Emerson MX <emersonmx@gmail.com>
2018-02-19 23:35:01 -0300

    Update wine scripts

--- wine/remove_extensions.sh
+++ wine/remove_extensions.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
-find ~/.local/share -name "*wine*" | xargs --no-run-if-empty rm -r
+rm -f ~/.local/share/applications/wine-extension*.desktop
+rm -f ~/.local/share/icons/hicolor/*/*/application-x-wine-extension*
+
+rm -f ~/.local/share/applications/mimeinfo.cache
+rm -f ~/.local/share/mime/packages/x-wine*
+rm -f ~/.local/share/mime/application/x-wine-extension*
 
 update-desktop-database ~/.local/share/applications
 update-mime-database ~/.local/share/mime/
