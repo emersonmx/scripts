deca1e39559507250cd00be4b310db28c51068b2
Emerson MX <emersonmx@gmail.com>
2018-02-24 05:25:24 -0300

    Remove optimize

--- laravel/simple-setup-laravel-dev.sh
+++ laravel/simple-setup-laravel-dev.sh
@@ -8,7 +8,6 @@ composer require --dev barryvdh/laravel-ide-helper
 php artisan clear-compiled
 php artisan ide-helper:generate
 php artisan ide-helper:meta
-php artisan optimize
 
 echo '_ide_helper.php' >> .gitignore
 echo '.phpstorm.meta.php' >> .gitignore
