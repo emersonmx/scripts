14ee22d80565cced6c7edd9b62a3829bf627a12b
Emerson MX <emersonmx@gmail.com>
2018-11-21 15:07:59 -0300

    Add files for vimopen

--- vim/vimopen.sh
+++ vim/vimopen.sh
@@ -5,12 +5,6 @@ IFS=$'\n\t'
 SHELL=/bin/zsh
 EDITOR=nvim
 
-if ! [ -x "$(command -v notify-send)" ]
-then
-    echo 'notify-send não encontrado!'
-    exit
-fi
-
 if ! [ -x "$(command -v tmux)" ]
 then
     echo 'TMUX não encontrado!'
@@ -23,6 +17,5 @@ then
     exit
 fi
 
-notify-send "Arquivo aberto no tmux"
 wmctrl -a $SHELL
 tmux new-window $EDITOR $@
