029218ecb368f3af6bb2fb2a1eb9d997f7148bdf
Emerson MX <emersonmx@gmail.com>
2018-11-21 15:00:07 -0300

    Update vimopen.sh

--- vim/vimopen.sh
+++ vim/vimopen.sh
@@ -2,10 +2,27 @@
 set -euo pipefail
 IFS=$'\n\t'
 
+SHELL=/bin/zsh
+EDITOR=nvim
+
+if ! [ -x "$(command -v notify-send)" ]
+then
+    echo 'notify-send não encontrado!'
+    exit
+fi
+
 if ! [ -x "$(command -v tmux)" ]
 then
     echo 'TMUX não encontrado!'
     exit
 fi
 
+if ! [ -x "$(command -v wmctrl)" ]
+then
+    echo 'wmctrl não encontrado!'
+    exit
+fi
+
+notify-send "Arquivo aberto no tmux"
+wmctrl -a $SHELL
 tmux new-window $EDITOR $@
