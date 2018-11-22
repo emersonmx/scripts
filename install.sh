ea7aec8aa8700239a0b4860a9fa3456f762fa20a
Emerson MX <emersonmx@gmail.com>
2018-11-22 06:15:15 -0300

    Rename vimopen.sh to vopen

--- install.sh
+++ install.sh
@@ -4,5 +4,5 @@ IFS=$'\n\t'
 
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-(cd ~/.local/bin && ln -sf $SCRIPT_DIR/vim/vimopen.sh)
-(cd ~/.local/share/applications && ln -sf $SCRIPT_DIR/vim/vimeclipse.sh)
+(cd ~/.local/bin && ln -sf $SCRIPT_DIR/vim/vopen)
+(cd ~/.local/share/applications && ln -sf $SCRIPT_DIR/vim/vopen.desktop)
