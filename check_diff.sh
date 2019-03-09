4e0e9a5509c4fd674f90f6328df03c89a12a8960
Emerson MX <emersonmx@gmail.com>
2019-03-08 21:08:41 -0300

    Update check_diff script

--- check_diff.sh
+++ check_diff.sh
@@ -2,30 +2,50 @@
 
 use_editor=
 
-if [[ $# == 2 ]]; then
+if [[ $# == 2 ]]
+then
     use_editor=echo
-elif [[ $# == 3 ]]; then
-    use_editor=$3
+elif [[ $# -ge 3 ]]
+then
+    use_editor=${@:3}
 else
-    echo "Uso: $0 <src> <dst> [<diff_editor>]"
+    echo "Uso: $0 <source> <dest> [<diff_editor>]"
     exit 0
 fi
 
-IFS="
-"
+source="$1"
+dest="$2"
 
-src=2
-dest=4
-grep_str='Only'
-if [[ $LANG == *"pt_BR"* ]]; then
-    src=3
-    dest=5
-    grep_str='Somente'
-fi
-
-for e in `diff -q -r "$1" "$2" | grep -v "$grep_str"`
+for f in $(find $source -follow \
+    -path "*/." -prune -o \
+    -path "*/.git" -prune -o \
+    -follow -print \
+)
 do
-    file_a=`echo $e | awk "{print $"$src"}"`
-    file_b=`echo $e | awk "{print $"$dest"}"`
-    $use_editor $file_a $file_b
+    source_file="$(echo $f | sed 's#^\./##')"
+    dest_file="$dest$source_file"
+
+    if [[ -d $f ]]
+    then
+        continue
+    fi
+    cmp --silent $source_file $dest_file && continue
+
+    if [[ $use_editor == 'echo' ]]
+    then
+        echo "$source_file <-> $dest_file"
+    else
+        edit="no"
+
+        read -p "Compare files $source_file <-> $dest_file? (y/N) " yn
+        case $yn in
+            [Yy]* ) edit="yes"; ;;
+            [Nn]* ) edit="no"; ;;
+        esac
+
+        if [[ $edit = "yes" ]]
+        then
+            $use_editor $source_file $dest_file
+        fi
+    fi
 done
