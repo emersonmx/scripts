68a63f53bdc6b02ff8ee9fc4c607f61277b9f18c
Emerson MX <emersonmx@gmail.com>
2019-09-15 12:21:02 -0300

    Update toggle_panel.sh

--- system/toggle_panel.sh
+++ system/toggle_panel.sh
@@ -2,13 +2,14 @@
 
 panel='panel-1'
 
-set_panel_autohide_behavior() {
-    xfconf-query -c xfce4-panel -p "/panels/$panel/autohide-behavior" -s $1
+function panel_autohide_behavior() {
+    xfconf-query -c xfce4-panel -p "/panels/$panel/autohide-behavior" $@
 }
-value=$(xfconf-query -c xfce4-panel -p "/panels/$panel/autohide-behavior")
-if [[ value -eq 0 ]]; then
-    set_panel_autohide_behavior 2
+
+value=$(panel_autohide_behavior)
+if [[ value -eq 0 ]]
+then
+    panel_autohide_behavior -s 2
 else
-    set_panel_autohide_behavior 1
-    set_panel_autohide_behavior 0
+    panel_autohide_behavior -s 0
 fi
