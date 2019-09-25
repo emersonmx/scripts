365565533ef51a134c2e09981a49f14640c65149
Emerson MX <emersonmx@gmail.com>
2019-09-24 23:29:00 -0300

    Add sudo to grub scripts

--- grub/grub-install.sh
+++ grub/grub-install.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=GRUB
+sudo grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=GRUB
--- grub/grub-mkconfig.sh
+++ grub/grub-mkconfig.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-grub-mkconfig -o /boot/grub/grub.cfg
+sudo grub-mkconfig -o /boot/grub/grub.cfg
