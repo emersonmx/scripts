60ca99349822ce35278aabc605137a5b1f05038f
Emerson MX <emersonmx@gmail.com>
2019-12-13 21:09:38 -0300

    Move alacritty to window_switcher

--- alacritty/window_switcher/alacritty.sh
+++ alacritty/window_switcher/alacritty.sh
@@ -5,4 +5,4 @@ script_dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 export ZSH_TMUX_AUTOSTART=false
 alacritty \
     --config-file "$script_dir/alacritty.yml" \
-    -e $script_dir/window_switcher.sh
+    -e "$script_dir/window_switcher.sh"
