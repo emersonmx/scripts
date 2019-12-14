caa8d24f3c4543232ba129946d32a8863a410c38
Emerson MX <emersonmx@gmail.com>
2019-12-13 21:02:24 -0300

    Update alacritty fzf

--- alacritty/window_switcher.sh
+++ alacritty/window_switcher.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
+id_regex="0x[a-f0-9]+"
 window=$(wmctrl -l \
     | awk '{$2=$3=""; print $0}' \
     | sed -r \
-        -e '/0x[a-f0-9]+\s+(Área de trabalho|Desktop|FZF)$/d' \
+        -e '/^'$id_regex'\s+Área de trabalho$/Id' \
+        -e '/^'$id_regex'\s+Desktop$/Id' \
+        -e '/^'$id_regex'\s+FZF$/d' \
     | fzf --layout=reverse --with-nth 2.. \
-    | awk '{print $1}'
+    | awk '{print $1}' \
 )
 
 wmctrl -i -a $window
