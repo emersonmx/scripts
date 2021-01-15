9ec131145cf5e64800a7cb020f4485f9c0b071d0
Emerson MX <emersonmx@gmail.com>
2021-01-14 21:12:21 -0300

    Add script to map tablet to monitor

---
+++ system/tablet-map-to-monitor.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+set -euo pipefail
+IFS=$'\n\t'
+
+stylus_device=$(xsetwacom list devices | grep "type: STYLUS" | sed -r 's/.*id: ([0-9]+).*/\1/g')
+prop_ctm_id=$(xinput list-props 22 | grep 'Coordinate Transformation Matrix' | sed -r 's/.*\(([0-9]+)\).*/\1/')
+monitors=$(xrandr --listmonitors | sed '1 d' | awk '{print $4}' | head -n2)
+primary_monitor=$(echo $monitors | cut -d' ' -f1)
+secondary_monitor=$(echo $monitors | cut -d' ' -f2)
+
+option=$(printf "Primary\nSecondary\nBoth" | dmenu -p "How monitor to map the tablet? " -i)
+
+case $option in
+    Primary)
+        xsetwacom set $stylus_device MapToOutput $primary_monitor
+        ;;
+    Secondary)
+        echo Switch to secondary monitor
+        xsetwacom set $stylus_device MapToOutput $secondary_monitor
+        ;;
+    Both)
+        xinput set-prop $stylus_device $prop_ctm_id 1.000000, 0.000000, 0.000000, 0.000000, 1.000000, 0.000000, 0.000000, 0.000000, 1.000000
+        ;;
+esac
