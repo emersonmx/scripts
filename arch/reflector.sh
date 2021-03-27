e4596163a254ed047973c039d80f802c2e050f11
Emerson MX <emersonmx@gmail.com>
2021-03-27 00:43:20 -0300

    Move reflector script to arch

--- arch/reflector.sh
+++ arch/reflector.sh
@@ -4,6 +4,6 @@ DBUS_SESSION_BUS_ADDRESS=unix:path=/run/user/1000/bus
 
 /usr/bin/notify-send -i gtk-info "Atualizando lista de mirrors..."
 
-reflector --verbose -c BR --latest 100 --sort rate --save /etc/pacman.d/mirrorlist
+reflector --verbose -c BR,US --latest 100 --sort rate --save /etc/pacman.d/mirrorlist
 
 /usr/bin/notify-send -i gtk-info "Lista de mirrors atualizada!"
