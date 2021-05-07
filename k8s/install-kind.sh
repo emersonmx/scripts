7e0f8a36bb10b4b8acfd6a13a0047159a044550e
Emerson Max de Medeiros Silva <emerson.silva@invillia.com>
2021-05-07 00:17:47 -0300

    Update k8s scripts

--- k8s/install-kind.sh
+++ k8s/install-kind.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
-latest_release="$(curl \
+version="$(curl \
     https://api.github.com/repos/kubernetes-sigs/kind/releases/latest \
         | jq -r .name)"
 tmp_binary="$(mktemp)"
 
-curl -Lo "$tmp_binary" "https://kind.sigs.k8s.io/dl/$latest_release/kind-linux-amd64"
+echo "Installing kind $version..."
+curl -Lo "$tmp_binary" "https://kind.sigs.k8s.io/dl/$version/kind-linux-amd64"
 chmod +x "$tmp_binary"
 mv "$tmp_binary" ~/.local/bin/kind
+echo "Done."
--- k8s/install-kubectl.sh
+++ k8s/install-kubectl.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
-latest_release="$(curl -L -s https://dl.k8s.io/release/stable.txt)"
+version="$(curl -L -s https://dl.k8s.io/release/stable.txt)"
 tmp_binary="$(mktemp)"
 
-curl -Lo "$tmp_binary" "https://dl.k8s.io/release/$latest_release/bin/linux/amd64/kubectl"
+echo "Installing kubectl $version..."
+curl -Lo "$tmp_binary" "https://dl.k8s.io/release/$version/bin/linux/amd64/kubectl"
 chmod +x "$tmp_binary"
 mv "$tmp_binary" ~/.local/bin/kubectl
+echo "Done."
