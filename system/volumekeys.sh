a2dcd2077a3f553656d4d8d72ea2058ef464654f
Emerson MX <emersonmx@gmail.com>
2021-07-31 00:03:01 -0300

    Add volumekeys script

---
+++ system/volumekeys.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+set -euo pipefail
+IFS=$'\n\t'
+
+# Arbitrary but unique message id
+msgId="991049"
+
+amixer set Master "$@" > /dev/null
+
+# Query amixer for the current volume and whether or not the speaker is muted
+volume="$(amixer get Master | tail -1 | awk '{print $5}' | sed 's/[^0-9]*//g')"
+mute="$(amixer get Master | tail -1 | awk '{print $6}' | sed 's/[^a-z]*//g')"
+if [[ $volume == 0 || "$mute" == "off" ]]; then
+    # Show the sound muted notification
+    dunstify -a "changeVolume" -u low -i audio-volume-muted -r "$msgId" "Volume muted"
+else
+    # Show the volume notification
+    dunstify -a "changeVolume" -u low -i audio-volume-high -r "$msgId" \
+    -h int:value:"$volume" "Volume: ${volume}%"
+fi
