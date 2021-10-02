7ae1e3af79ee392b748aa58cbb8f628027ac8697
Emerson Max de Medeiros Silva <emerson.silva@invillia.com>
2021-10-02 00:53:23 -0300

    Update setup_coc_local_settings.py

--- vim/python/setup_coc_local_settings.py
+++ vim/python/setup_coc_local_settings.py
@@ -5,17 +5,24 @@ from subprocess import run
 
 def main() -> None:
     vim_path = pathlib.Path(".vim")
-    vim_path.mkdir(exist_ok=True)
     coc_settings_path = vim_path / "coc-settings.json"
-    if not coc_settings_path.exists():
-        python_binary_path = (
-            run(["poetry", "run", "which", "python"], capture_output=True)
-            .stdout.decode()
-            .strip()
-        )
-        coc_settings = {"python.pythonPath": python_binary_path}
-        with open(".vim/coc-settings.json", "w+") as f:
-            json.dump(coc_settings, f, indent=4)
+    if coc_settings_path.exists():
+        print(f"{coc_settings_path} already exists!")
+        return
+
+    python_path_cmd = run(
+        "poetry run which python",
+        capture_output=True,
+        shell=True,
+    )
+    python_path_cmd.check_returncode()
+
+    python_binary_path = python_path_cmd.stdout.decode().strip()
+    coc_settings = {"python.pythonPath": python_binary_path}
+
+    vim_path.mkdir(exist_ok=True)
+    with open(".vim/coc-settings.json", "w+") as f:
+        json.dump(coc_settings, f, indent=4)
 
 
 if __name__ == "__main__":
