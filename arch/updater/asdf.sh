e612dbf714f04ab789a40d5efa2f7ac32d43b813
Emerson MX <emersonmx@gmail.com>
2022-01-22 14:41:33 -0300

    Add more tools

--- arch/updater/asdf.sh
+++ arch/updater/asdf.sh
@@ -39,33 +39,41 @@ export PATH=$(echo $PATH | sed "s#$USER_LOCAL/bin:##")
 asdf update
 asdf plugin update --all
 
-# Python
+# Languages
+## Python
 install_tool python
 python -m pip install --upgrade pip
 cat ~/.default-python-packages | xargs -I _ python -m pip install --upgrade _
 asdf_reshim_tool python
 
-# NodeJS
+## NodeJS
 install_tool nodejs
 cat ~/.default-npm-packages | xargs -I _ npm install -g _
 npm update -g
 asdf_reshim_tool nodejs
 
-# Golang
+## Golang
 install_tool golang
 cat ~/.default-golang-pkgs | xargs -I _ go install _
 asdf_reshim_tool golang
 
-# Rust
+## Rust
 asdf_uninstall_tool rust stable
 install_tool rust stable
 
-# Lua
+## Lua
 install_tool lua
 cat ~/.default-lua-packages | xargs -I _ bash -c "luarocks install _"
 asdf_reshim_tool lua
 
-# direnv
+# Tools
+## direnv
 install_tool direnv
+
+## k3d
+install_tool k3d
+
+## kubectl
+install_tool kubectl
 
 export PATH="$_old_path"
