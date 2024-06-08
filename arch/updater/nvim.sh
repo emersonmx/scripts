702f836b25c55bf87269b7debd2a7ee46c1986c6
Emerson MX <emersonmx@gmail.com>
2024-06-08 15:56:01 -0300

    [arch] Update nvim update script

--- arch/updater/nvim.sh
+++ arch/updater/nvim.sh
@@ -1,10 +1,4 @@
 #!/bin/bash
 
-nvim \
-    --noplugin \
-    -u "$HOME/.config/nvim/lua/emersonmx/packer.lua" \
-    -c "PackerSync"
-
-nvim --headless -c "MasonUpdate" -c "qall"
-nvim --headless -c "TSUpdateSync" -c "qall"
-nvim -c "Mason"
+cd "$HOME/.config/nvim/" || exit
+./setup.sh
