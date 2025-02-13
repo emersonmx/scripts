ccb55cc5b62a0cbedcba29833d837e22309ee398
Emerson MX <emersonmx@gmail.com>
2025-02-13 10:26:55 -0300

    [ubuntu] Update misc script

--- ubuntu/updater/misc.py
+++ ubuntu/updater/misc.py
@@ -13,6 +13,7 @@ HOME_LOCAL_BIN = HOME_LOCAL / "bin"
 def main() -> int:
     update_direnv()
     update_k3d()
+    update_k6()
     update_kubectl()
     update_sccache()
 
@@ -45,6 +46,53 @@ def update_k3d() -> None:
     print("Done.\n")
 
 
+def update_k6() -> None:
+    print("Installing k6...")
+
+    platform = "linux-amd64"
+    repo_url = "https://github.com/grafana/k6"
+    cmd = " | ".join(
+        [
+            f"git ls-remote --tags --refs '{repo_url}'",
+            "grep -o 'refs/tags/.*'",
+            "sed 's#^refs/tags/v##g'",
+        ]
+    )
+    cmd = f"npx semver `{cmd}` | tail -n1"
+    version = "v" + (
+        run(
+            cmd,
+            shell=True,
+            capture_output=True,
+        )
+        .stdout.decode()
+        .strip()
+    )
+
+    pkg_file = f"k6-{version}-{platform}.tar.gz"
+    url = f"{repo_url}/releases/download/{version}/{pkg_file}"
+
+    bin_path = HOME_LOCAL_BIN / "k6"
+    bin_tmp_dir = Path(tempfile.mkdtemp())
+    bin_tmp_file = bin_tmp_dir / pkg_file
+    run(["curl", "-fsSL", "-o", bin_tmp_file, "-C", "-", url])
+    run(
+        [
+            "tar",
+            "-xzf",
+            bin_tmp_file,
+            "-C",
+            bin_tmp_dir,
+            "--strip-components=1",
+        ]
+    )
+    run(["mv", "-f", bin_tmp_dir / "k6", bin_path])
+    run(["rm", "-rf", bin_tmp_dir])
+    bin_path.chmod(0o755)
+
+    print("Done.\n")
+
+
 def update_kubectl() -> None:
     print("Installing kubectl...")
 
@@ -65,7 +113,7 @@ def update_kubectl() -> None:
             f"https://dl.k8s.io/release/{version}/bin/linux/amd64/kubectl",
         ]
     )
-    kubectl_path.chmod(0o775)
+    kubectl_path.chmod(0o755)
 
     print("Done.\n")
 
@@ -92,28 +140,26 @@ def update_sccache() -> None:
         .stdout.decode()
         .strip()
     )
-    url = (
-        f"{repo_url}/releases/download/"
-        f"{version}/sccache-{version}-{platform}.tar.gz"
-    )
+    pkg_file = f"sccache-{version}-{platform}.tar.gz"
+    url = f"{repo_url}/releases/download/{version}/{pkg_file}"
 
-    sccache_path = HOME_LOCAL_BIN / "sccache"
-    sccache_tmp_dir = Path(tempfile.mkdtemp())
-    sccache_tmp_file = sccache_tmp_dir / "sccache.tar.gz"
-    run(["curl", "-fsSL", "-o", sccache_tmp_file, "-C", "-", url])
+    bin_path = HOME_LOCAL_BIN / "sccache"
+    bin_tmp_dir = Path(tempfile.mkdtemp())
+    bin_tmp_file = bin_tmp_dir / pkg_file
+    run(["curl", "-fsSL", "-o", bin_tmp_file, "-C", "-", url])
     run(
         [
             "tar",
             "-xzf",
-            sccache_tmp_file,
+            bin_tmp_file,
             "-C",
-            sccache_tmp_dir,
+            bin_tmp_dir,
             "--strip-components=1",
         ]
     )
-    run(["rm", sccache_tmp_file])
-    run(["mv", "-f", sccache_tmp_dir / "sccache", sccache_path])
-    sccache_path.chmod(0o775)
+    run(["mv", "-f", bin_tmp_dir / "sccache", bin_path])
+    run(["rm", "-rf", bin_tmp_dir])
+    bin_path.chmod(0o755)
 
     print("Done.\n")
 
